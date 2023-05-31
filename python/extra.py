cece0daffe75e31e744bc3ae4f895af333eb51a9
XanderVR04 <xander.vr@outlook.com>
2023-05-31 11:45:30 +0200

    tweede commit

--- python/extra.py
+++
@@ -1,8 +0,0 @@
-  #def add_bike(self, bike):
-   # self.bikes.append(bike)
-
-  #def remove_bike(self, bike_id):
-   # for bike in self.bikes:
-    #  if bike.id == bike_id:
-     #   self.bikes.remove(bike)
-      #  break
